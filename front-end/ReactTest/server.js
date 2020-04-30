da73cceb95b3b4925c25eb24c4d2cbbc273fe44f
monica.sadafule@neosofttech.com <monica.sadafule@neosofttech.com>
2020-04-30 18:02:55 +0530

    cors enabled for api access

--- front-end/ReactTest/server.js
+++ front-end/ReactTest/server.js
@@ -3,6 +3,13 @@ const countries = require("./countries.json")
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // GET countries
